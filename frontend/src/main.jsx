04d9dcab69ea91d2d8f5a32a56b77713274f3b94
Harshiiitaaa <garg2004harshita@gmail.com>
2025-08-23 11:45:14 +0530

    working fine

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,11 @@
+// main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './pages/App'
+import App from './App.jsx'   // not inside pages, just directly from src folder
 import './index.css'
-import Dashboard from './pages/Dashboard'
-import Profile from './pages/Profile'
-import Practice from './pages/Practice'
-
-const router = createBrowserRouter([
-  { path: '/', element: <App />,
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: 'profile', element: <Profile /> },
-      { path: 'practice', element: <Practice /> }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
